54dd681728f2057fa12144f23b789ff39525ae21
shakedguy <shakedguy94@gmail.com>
2022-06-24 20:58:26 +0300

    update

--- utils/serviceAccounts.js
+++ utils/serviceAccounts.js
@@ -1,4 +1,4 @@
-const firebaseAdminServiceAccount = JSON.parse(env.FIREBASE_SERVICE_ACCOUNT);
+const firebaseAdminServiceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 const bigQueryServiceAccount = firebaseAdminServiceAccount;
 
 export { firebaseAdminServiceAccount, bigQueryServiceAccount };
