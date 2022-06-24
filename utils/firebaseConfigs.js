5f48f1e904a9912a7916b707a8052c42638946c5
shakedguy <shakedguy94@gmail.com>
2022-06-24 16:48:33 +0300

    update

--- utils/firebaseConfigs.js
+++ utils/firebaseConfigs.js
@@ -1,11 +1,22 @@
 const env = process.env;
+// export const firebaseConfig = {
+// 	apiKey: env.FIREBASE_API_KEY,
+// 	authDomain: env.FIREBASE_AUTH_DOMAIN,
+// 	databaseURL: env.FIREBASE_DATABASE_URL,
+// 	projectId: env.FIREBASE_PROJECT_ID,
+// 	storageBucket: env.FIREBASE_STORAGE_BUCKE,
+// 	messagingSenderId: env.FIREBASE_MESSAGEING_SENDER_ID,
+// 	appId: env.FIREBASE_APP_ID,
+// 	measurementId: env.FIREBASE_MEASUREMENT_ID,
+// };
+
 export const firebaseConfig = {
-	apiKey: env.FIREBASE_API_KEY,
-	authDomain: env.FIREBASE_AUTH_DOMAIN,
-	databaseURL: env.FIREBASE_DATABASE_URL,
-	projectId: env.FIREBASE_PROJECT_ID,
-	storageBucket: env.FIREBASE_STORAGE_BUCKE,
-	messagingSenderId: env.FIREBASE_MESSAGEING_SENDER_ID,
-	appId: env.FIREBASE_APP_ID,
-	measurementId: env.FIREBASE_MEASUREMENT_ID,
+	apiKey: 'AIzaSyCuIiT0HI7BK1xVxUbAcv6FKTmuRtrpTKw',
+	authDomain: 'react-login-bd9ed.firebaseapp.com',
+	databaseURL: 'https://react-login-bd9ed-default-rtdb.firebaseio.com',
+	projectId: 'react-login-bd9ed',
+	storageBucket: 'react-login-bd9ed.appspot.com',
+	messagingSenderId: '795993175585',
+	appId: '1:795993175585:web:3104d973caf64b8599d560',
+	measurementId: 'G-LF00S1YVLG',
 };
